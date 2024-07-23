7a77d659952e751eab009f23b7ec12fb2f089f77
Kinda Hall <1142704468@qq.com>
2024-07-23 10:59:40 +0800

    feat: 🎸 增加模型文件夹整理功能

--- bin/main.py
+++ bin/main.py
@@ -19,6 +19,43 @@ def get_model_info(path: Annotated[str, typer.Option("--path", "-p", help="Path 
         print("未找到模型信息")
     else:
         print(model_info)
+        
+@app.command('classify', help='Classify model type.')
+def classify_model(path: Annotated[str, typer.Option("--path", "-p", help="Path to the model file")] = os.getcwd(),
+                   recursion: Annotated[bool, typer.Option("--recursion", "-r", help="If you want to classify the models in sub folder")] = False):
+    total = 0
+    for dirpath, dirnames, filenames in os.walk(path):
+        for item in filenames:
+            file_item = Path(dirpath) / item
+            if(file_item.suffix == '.safetensors' or file_item.suffix == '.ckpt'):
+                total += 1
+        if not recursion:
+            dirnames[:] = []  # Skip subdirectories in this git repository
+    print(f"共计 {total} 个模型需要被整理。")
+    
+    progress_bar = tqdm(total=total)
+    for dirpath, dirnames, filenames in os.walk(path):
+        for item in filenames:
+            file_item = Path(dirpath) / item
+            if(file_item.suffix == '.safetensors' or file_item.suffix == '.ckpt'):
+                file_item = Path(dirpath) / item
+                model_hash = calculate_file_hash(file_item)
+                model_info = get_civitai_model_info_by_hash(model_hash)
+                # 创建对应模型类型的文件夹
+                os.makedirs(f"{file_item.parent}/{model_info['baseModel']}", exist_ok=True)
+                file_item.rename(f"{file_item.parent}/{model_info['baseModel']}/{file_item.name}")
+                # 找到对应模型文件的预览图片
+                preview_file = file_item.with_suffix(".preview.png")
+                # 移动图片到对应的文件夹
+                if preview_file.exists():
+                    preview_file.rename(f"{file_item.parent}/{model_info['baseModel']}/{file_item.stem}.preview.png")
+                else:
+                    preview_file = file_item.with_suffix(".png")
+                    if preview_file.exists():
+                        preview_file.rename(f"{file_item.parent}/{model_info['baseModel']}/{file_item.stem}.png")
+            progress_bar.update(1)
+        if not recursion:
+            dirnames[:] = []  # Skip subdirectories in this git repository
 
 @app.command('preview', help='Get model preview image.')
 def preview_model(path: Annotated[str, typer.Option("--path", "-p", help="The path to the directory")] = os.getcwd(),
