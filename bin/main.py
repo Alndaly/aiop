0b1aa898680cdb29fc3aac3f1721c5e9f22d431e
Kinda Hall <1142704468@qq.com>
2024-07-23 11:09:50 +0800

    fix: 🐛 修复遇到c站哈希库没有的model时会报错的问题 增加classify脚本自动下载预览图片功能

--- bin/main.py
+++ bin/main.py
@@ -22,7 +22,8 @@ def get_model_info(path: Annotated[str, typer.Option("--path", "-p", help="Path 
         
 @app.command('classify', help='Classify model type.')
 def classify_model(path: Annotated[str, typer.Option("--path", "-p", help="Path to the model file")] = os.getcwd(),
-                   recursion: Annotated[bool, typer.Option("--recursion", "-r", help="If you want to classify the models in sub folder")] = False):
+                   recursion: Annotated[bool, typer.Option("--recursion", "-r", help="If you want to classify the models in sub folder")] = False,
+                   auto_download_preview_file: Annotated[bool, typer.Option("--auto-download-preview-file", "-a", help="Download the preview image automatically")] = False):
     total = 0
     for dirpath, dirnames, filenames in os.walk(path):
         for item in filenames:
@@ -41,6 +42,10 @@ def classify_model(path: Annotated[str, typer.Option("--path", "-p", help="Path 
                 file_item = Path(dirpath) / item
                 model_hash = calculate_file_hash(file_item)
                 model_info = get_civitai_model_info_by_hash(model_hash)
+                if model_info == None:
+                    print(f'模型{item}在c站上无法找到预览图，可能是对应hash值仍未存储入c站模型hash库')
+                    progress_bar.update(1)
+                    continue
                 # 创建对应模型类型的文件夹
                 os.makedirs(f"{file_item.parent}/{model_info['baseModel']}", exist_ok=True)
                 file_item.rename(f"{file_item.parent}/{model_info['baseModel']}/{file_item.name}")
@@ -53,6 +58,15 @@ def classify_model(path: Annotated[str, typer.Option("--path", "-p", help="Path 
                     preview_file = file_item.with_suffix(".png")
                     if preview_file.exists():
                         preview_file.rename(f"{file_item.parent}/{model_info['baseModel']}/{file_item.stem}.png")
+                    else:
+                        if auto_download_preview_file:
+                            # get the preview image of model
+                            model_preview_image_url = model_info['images'][0]['url']
+                            response = httpx.get(model_preview_image_url)
+                            preview_path = file_item.with_suffix(".preview.png")
+                            if response.status_code == 200:
+                                with open(preview_path, "wb") as f:
+                                    f.write(response.content)
             progress_bar.update(1)
         if not recursion:
             dirnames[:] = []  # Skip subdirectories in this git repository
